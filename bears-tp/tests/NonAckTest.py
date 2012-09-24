acd9df25a568fe2475348caf81e596c1249e3634
Bobby Shane <rshane00@gmail.com>
2012-09-24 14:36:12 -0700

    getting tests to work

--- bears-tp/tests/NonAckTest.py
+++ bears-tp/tests/NonAckTest.py
@@ -6,11 +6,11 @@ from BasicTest import *
 #Hits the sender with packs that aren't acks... spooky
 
 class NonAckTest(BasicTest):
-    def handle_packet(self)
+    def handle_packet(self):
         for p in self.forwarder.in_queue:
             if p.msg_type=="ack":
                 if random.choice([True,False]):
-                    p.update_packet(msg_type=data, seqno=p.seqno, 
+                    p.update_packet(msg_type='data', seqno=p.seqno, 
                                     data='I am a data packet', full_packet=None) 
                 self.forwarder.out_queue.append(p)
             else:
